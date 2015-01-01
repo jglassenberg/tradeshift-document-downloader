5bddd9a26a8d71bb6456dde5dc3bed986adde045
jglassenberg <jglassenberg@gmail.com>
2015-01-01 15:52:20 -0800

    Update tradeshift.rb
    Added proper headers to make TS API Calls

--- lib/tradeshift.rb
+++ lib/tradeshift.rb
@@ -17,7 +17,15 @@ class Tradeshift
 
         # call API to find out how many docs are in the list
         #TODO: add parameters to coincide with selected filters
-        response = HTTParty.get('https://api.tradeshift.com/tradeshift/rest/external/documents/')
+        response = HTTParty.get(
+        	'https://api.tradeshift.com/tradeshift/rest/external/documents/', 
+        	headers: {
+    	    		'X-Tradeshift-TenantId' => company_account_id,
+    	    		'Accept' => 'application/json',
+    	    		'User-Agent' => 'TradeshiftDocumentsDownload/0.1',
+    	    		'Authorization' => "Bearer #@accessToken"
+    	    	}
+        )
         parsed = JSON.parse(response)
 
         #Store all of the documents in an array.  It will be an array of pages, 25 items long.
@@ -32,11 +40,20 @@ class Tradeshift
 
         # loop through until you have all content.  If there was only one page, this doesn't execute.
         (numPages - 1).times do |i|
-	        #Call API with the page #i
-	        #TODO: this call may require filters based on the customer's specifications
-    	    @esponse = HTTParty.get("https://api.tradeshift.com/tradeshift/rest/external/documents/?page=#{i}&limit=#{itemsperpage}")
-	        parsed = JSON.parse(response)
-	        results.push(parsed["Document"])
+	    #Call API with the page #i
+	    #TODO: this call may require filters based on the customer's specifications
+    	    response = HTTParty.get(
+    	    	"https://api.tradeshift.com/tradeshift/rest/external/documents/?page=#{i}&limit=#{itemsperpage}", 
+    	    	headers: {
+    	    		'X-Tradeshift-TenantId' => company_account_id,
+    	    		'Accept' => 'application/json',
+    	    		'User-Agent' => 'TradeshiftDocumentsDownload/0.1',
+    	    		'Authorization' => "Bearer #@accessToken"
+    	    	}
+    	    )
+
+	    parsed = JSON.parse(response)
+	    results.push(parsed["Document"])
         end
         return results
     end
