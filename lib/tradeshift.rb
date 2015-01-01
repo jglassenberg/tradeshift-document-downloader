0960ace88d179df4ed838d9f5da4571dbf3d76b8
jglassenberg <jglassenberg@gmail.com>
2015-01-01 15:09:54 -0800

    Update tradeshift.rb
    Frame for calling the external/documents API, looping through the number of documents in the account

--- lib/tradeshift.rb
+++ lib/tradeshift.rb
@@ -15,5 +15,29 @@ class Tradeshift
     # notes: Get all the pages.
     def documents_list_for_company(company_account_id)
 
+        # call API to find out how many docs are in the list
+        #TODO: add parameters to coincide with selected filters
+        response = HTTParty.get('https://api.tradeshift.com/tradeshift/rest/external/documents/')
+        parsed = JSON.parse(response)
+
+        #Store all of the documents in an array.  It will be an array of pages, 25 items long.
+        results = Array.new()
+        results.push(parsed["Document"])
+
+        #get the length of the app
+        numpages = parsed["numPages"]
+
+        #We can change this easily, but keep the items per page to 25 for now (the default)
+        itemsperpage = 25 
+
+        # loop through until you have all content.  If there was only one page, this doesn't execute.
+        (numPages - 1).times do |i|
+	        #Call API with the page #i
+	        #TODO: this call may require filters based on the customer's specifications
+    	    @esponse = HTTParty.get("https://api.tradeshift.com/tradeshift/rest/external/documents/?page=#{i}&limit=#{itemsperpage}")
+	        parsed = JSON.parse(response)
+	        results.push(parsed["Document"])
+        end
+        return results
     end
 end
