e26da1d1a1aef9cc57d80b8b284c00c90103fb0e
Jordan Feldstein <jfeldstein@gmail.com>
2015-01-01 14:49:48 -0800

    Implement document update job

--- lib/jobs/update_company_documents.rb
+++ lib/jobs/update_company_documents.rb
@@ -1,7 +1,25 @@
 class Jobs::UpdateCompanyDocuments
     class << self
-        def run(company)
+        def run(ts, company)
+            live_docs = ts.documents_list_for_company(company.ts_account_id)
 
+            # Update documents with new data
+            live_docs.each do |doc|
+                # Find the cached document
+                match = company.documents.where{id==my{doc['id']}}
+
+                # Cache the document if missed.
+                match ||= Document.create_from_company_and_response(company, doc)
+
+                match.update_from_response(doc)
+            end
+
+            # Remove documents that no longer exist
+            live_ids = live_docs.map{|doc| doc['DocumentId']}
+            company.documents.where{ts_doc_id << my{live_ids}}.each{|doc| doc.destroy }
+
+            company.docs_last_updated_at = Time.now
+            company.save!
         end
 
         handle_asynchronously :run
