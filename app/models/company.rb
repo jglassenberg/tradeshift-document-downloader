74b0a6d53368bcbe10b9d67986920eb4a60a0355
Jordan Feldstein <jfeldstein@gmail.com>
2015-01-01 17:20:31 -0800

    Fetch data after creating new company

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,11 +2,18 @@ class Company < ActiveRecord::Base
   has_many :documents
   has_many :users
 
+  after_create :refresh_data
+
   def access_token
     token = users.where{oauth_token!=nil}.first.try(:oauth_token)
 
     raise "Company has no access token!" in token.nil?
 
     token
+  end
+
+  def refresh_data
+    ts = Tradeshift.new access_token
+    Jobs::UpdateCompanyDocuments.run ts, self
   end
 end
