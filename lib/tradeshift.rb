72679e61edc3c79dee73f9cc8fe352c0e5ae0e28
Jordan Feldstein <jfeldstein@gmail.com>
2015-01-01 18:19:50 -0800

    TS library cleanup a little

--- lib/tradeshift.rb
+++ lib/tradeshift.rb
@@ -12,10 +12,9 @@ class Tradeshift
     # params: A CompanyAccountId for the company whose docs we want.
     # returns: A list of all the documents owned by this company.
     # notes: Get all the pages.
-    def documents_list_for_company(company_account_id)
+    def documents_list_for_company()
 
         # call API to find out how many docs are in the list
-        #TODO: add parameters to coincide with selected filters
         response = HTTParty.get(
         	'https://api.tradeshift.com/tradeshift/rest/external/documents/',
         	headers: {
@@ -40,8 +39,8 @@ class Tradeshift
 
         # loop through until you have all content.  If there was only one page, this doesn't execute.
         (numPages - 1).times do |i|
-	    #Call API with the page #i
-	    #TODO: this call may require filters based on the customer's specifications
+    	    #Call API with the page #i
+    	    #TODO: this call may require filters based on the customer's specifications
     	    response = HTTParty.get(
     	    	"https://api.tradeshift.com/tradeshift/rest/external/documents/?page=#{i}&limit=#{itemsperpage}",
     	    	headers: {
@@ -52,9 +51,8 @@ class Tradeshift
     	    	}
     	    )
 
-	    parsed = JSON.parse(response)
-	    #results.push(parsed["Document"])
-	    results += parsed["Document"]
+            parsed = JSON.parse(response)
+	        results += parsed["Document"]
         end
 
         #How to obtain data:
